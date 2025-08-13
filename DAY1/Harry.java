ab9187219410c966429a2b4f1600333293a64eaf
rue <thenautiyal01@gmail.com>
2025-08-13 11:29:47 +0530

    [Sakshi Nautiyal] add. DAY1

---
+++ DAY1/Harry.java
@@ -0,0 +1,10 @@
+public class Harry {
+    public static void main(String[] args) {
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in " + currentYear + " is " + age);
+    }
+}
