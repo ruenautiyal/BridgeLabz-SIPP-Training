01895df14ee9232287d322d9a2457cd0d94bf7c6
rue <thenautiyal01@gmail.com>
2025-08-13 12:36:18 +0530

    [Sakshi Nautiyal] add. DAY5

---
+++ DAY5/SimpleInterest.java
@@ -0,0 +1,19 @@
+package DAY5;
+
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Principal: ");
+        double principal = sc.nextDouble();
+        System.out.print("Enter Rate: ");
+        double rate = sc.nextDouble();
+        System.out.print("Enter Time: ");
+        double time = sc.nextDouble();
+        double si = (principal * rate * time) / 100;
+        System.out.println("The Simple Interest is " + si + " for Principal " + principal + ", Rate of Interest " + rate
+                + " and Time " + time);
+        sc.close();
+    }
+}
