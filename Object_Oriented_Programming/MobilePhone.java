69206298d8425ee013e171032dcd8b8a39f6672b
rue <thenautiyal01@gmail.com>
2025-08-25 21:42:04 +0530

    [Sakshi Nautiyal] added MobilePhone.java

---
+++ Object_Oriented_Programming/MobilePhone.java
@@ -0,0 +1,24 @@
+package Object_Oriented_Programming;
+
+class MobilePhone {
+    String brand;
+    String model;
+    double price;
+
+    MobilePhone(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        MobilePhone phone = new MobilePhone("Infinix", "HOT 11s", 9000);
+        phone.displayDetails();
+    }
+}
