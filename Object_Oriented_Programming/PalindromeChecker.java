bf97828f43c8e73d28ec0d18993e602c9b56ac77
rue <thenautiyal01@gmail.com>
2025-08-25 21:43:36 +0530

    [Sakshi Nautiyal] added PalindromeChecker.java

---
+++ Object_Oriented_Programming/PalindromeChecker.java
@@ -0,0 +1,28 @@
+package Object_Oriented_Programming;
+
+class PalindromeChecker {
+    String text;
+
+    PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    boolean isPalindrome() {
+        String cleaned = text.replaceAll("\\s+", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+        return cleaned.equals(reversed);
+    }
+
+    void displayResult() {
+        if (isPalindrome()) {
+            System.out.println("'" + text + "' is a palindrome.");
+        } else {
+            System.out.println("'" + text + "' is not a palindrome.");
+        }
+    }
+
+    public static void main(String[] args) {
+        PalindromeChecker checker = new PalindromeChecker("SOS");
+        checker.displayResult();
+    }
+}
