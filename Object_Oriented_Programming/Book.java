49f32178b378d5b03d4901f224a0deb1ee935193
rue <thenautiyal01@gmail.com>
2025-08-25 21:38:19 +0530

    [Sakshi Nautiyal] added Book.java

---
+++ Object_Oriented_Programming/Book.java
@@ -0,0 +1,24 @@
+package Object_Oriented_Programming;
+
+public class Book {
+    String title;
+    String author;
+    double price;
+
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void displayBookDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book("Java Programming", "James", 500);
+        book.displayBookDetails();
+    }
+}
